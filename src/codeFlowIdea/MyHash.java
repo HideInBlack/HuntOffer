d3a6794a9f840bf3c058b4951ed7dc674728a273
dzy_git <1327293674@qq.com>
2023-10-19 18:07:22 +0800

    first build the MyHash.java file and just do it!

---
+++ src/codeFlowIdea/MyHash.java
@@ -0,0 +1,19 @@
+package codeFlowIdea;
+
+/**
+ * codeFlowIdea 代码随想录学习记录 time：2023年10月19日18:02:16 ->
+ * author：董政宇
+ * 第三部分 哈希部分：MyHash
+ */
+public class MyHash {
+
+
+
+
+    /**
+     * -----------------------------------------------测试-----------------------------------------------
+     */
+    public static void main(String[] args) {
+
+    }
+}
