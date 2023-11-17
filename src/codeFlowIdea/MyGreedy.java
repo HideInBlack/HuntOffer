c2ae29b6b5ec38dec652fbecfd45ad4ea90658a0
dzy_git <1327293674@qq.com>
2023-11-17 21:35:25 +0800

    first build MyGreedy 【2023年11月17日21:35:23】

---
+++ src/codeFlowIdea/MyGreedy.java
@@ -0,0 +1,19 @@
+package codeFlowIdea;
+
+/**
+ * codeFlowIdea 代码随想录学习记录 time：2023年11月17日21:33:13 ->
+ * author：董政宇
+ * 第九部分 贪心部分：MyGreedy
+ */
+public class MyGreedy {
+
+
+
+
+    /**
+     * -----------------------------------------------测试-----------------------------------------------
+     */
+    public static void main(String[] args) {
+
+    }
+}
